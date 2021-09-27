1aece4b12d4010d94e8a096703f9d8dacd39a2ea
Simon Oulevay <simon.oulevay@gmail.com>
2021-09-27 17:33:56 +0200

    Add moves table

--- priv/repo/migrations/20210921151550_initial_schema.exs
+++ priv/repo/migrations/20210921151550_initial_schema.exs
@@ -8,9 +8,18 @@ defmodule Minesweeper.Repo.Migrations.InitialSchema do
     create table("games", primary_key: false) do
       add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
       add :state, :game_state, default: "ongoing", null: false
-      add :data, :map, null: false
+      add :bombs, {:array, :integer}, null: false
       add :version, :integer, default: 0, null: false
       timestamps(inserted_at: :created_at, type: :naive_datetime_usec, null: false)
     end
+
+    create table("moves", primary_key: false) do
+      add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
+      add :position, {:array, :integer}, null: false
+      add :game_id, references("games", type: :uuid, on_delete: :delete_all, on_update: :restrict)
+      timestamps(inserted_at: :played_at, updated_at: false, type: :naive_datetime_usec, null: false)
+    end
+
+    create index(:moves, [:game_id, :played_at])
   end
 end
