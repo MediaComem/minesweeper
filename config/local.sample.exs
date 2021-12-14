bd70bec58c22a4f7b3f73d4f4292add4784c5f22
Simon Oulevay <simon.oulevay@gmail.com>
2021-12-14 08:38:23 +0100

    Improve the local configuration sample

--- config/local.sample.exs
+++ config/local.sample.exs
@@ -1,19 +1,19 @@
 import Config
 
-# Configure the database connection URL (format is
-# "ecto://<username>[:<password>]@<host>[:<port>]/<database-name>").
+# Configure the database connection URL.
+#
+# The format is:  "ecto://<username>[:<password>]@<host>[:<port>]/<database-name>"
+# For example:    "ecto://minesweeper:mysecretpassword@localhost:5432/minesweeper"
 config :minesweeper, Minesweeper.Repo,
   url:
     if(config_env() != :test,
+      # Use a different database for development/production and for testing.
       do: "ecto://minesweeper:changeme@localhost/minesweeper",
-      # The MIX_TEST_PARTITION environment variable can be used to provide built-in
-      # test partitioning in CI environment. Run `mix help test` for more information.
-      else:
-        "ecto://minesweeper:changeme@localhost/minesweeper-test#{System.get_env("MIX_TEST_PARTITION")}"
+      else: "ecto://minesweeper:changeme@localhost/minesweeper-test"
     )
 
 # Configure the web endpoint.
 config :minesweeper, MinesweeperWeb.Endpoint,
-  # Use `mix phx.gen.secret` to generate a suitable value for the secret key
+  # Use "mix phx.gen.secret" to generate a suitable value for the secret key
   # base.
   secret_key_base: "changeme"
