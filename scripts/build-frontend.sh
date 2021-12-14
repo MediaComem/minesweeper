908b75593746744674adb38c46a702c33c0bf243
Simon Oulevay <simon.oulevay@gmail.com>
2021-12-14 09:25:25 +0100

    Add frontend.build mix alias to build the frontend

---
+++ scripts/build-frontend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+function fail() {
+  >&2 echo "Error: $@"
+  exit 1
+}
+
+command -v npm &>/dev/null || fail "npm command not found; did you install Node.js?"
+
+set -x
+
+cd assets
+npm run build
