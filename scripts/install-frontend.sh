0a5ae123794af1e0a0c43744a3a571ee8b80544f
Simon Oulevay <simon.oulevay@gmail.com>
2021-12-14 09:08:03 +0100

    Check for npm in frontend installation script

--- scripts/install-frontend.sh
+++ scripts/install-frontend.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-set -ex
+set -e
+
+function fail() {
+  >&2 echo "Error: $@"
+  exit 1
+}
+
+command -v npm &>/dev/null || fail "npm command not found; did you install Node.js?"
+
+set -x
+
 cd assets
 npm ci
