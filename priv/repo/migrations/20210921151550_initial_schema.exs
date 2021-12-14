9df26d0aa2da5dc3055b460ea6ad76c605482b8a
Simon Oulevay <simon.oulevay@gmail.com>
2021-12-14 08:38:07 +0100

    Create uuid-ossp extension only if it doesn't exist

--- priv/repo/migrations/20210921151550_initial_schema.exs
+++ priv/repo/migrations/20210921151550_initial_schema.exs
@@ -2,7 +2,7 @@ defmodule Minesweeper.Repo.Migrations.InitialSchema do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION \"uuid-ossp\";", "DROP EXTENSION \"uuid-ossp\";")
+    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";", "DROP EXTENSION \"uuid-ossp\";")
     execute("CREATE TYPE game_state AS ENUM ('ongoing', 'win', 'loss');", "DROP TYPE game_state;")
 
     create table("games", primary_key: false) do
@@ -18,7 +18,10 @@ defmodule Minesweeper.Repo.Migrations.InitialSchema do
     create table("moves", primary_key: false) do
       add :id, :uuid, primary_key: true, default: fragment("uuid_generate_v4()")
       add :position, {:array, :integer}, null: false
-      add :game_id, references("games", type: :uuid, on_delete: :delete_all, on_update: :restrict), null: false
+
+      add :game_id,
+          references("games", type: :uuid, on_delete: :delete_all, on_update: :restrict),
+          null: false
 
       timestamps(
         inserted_at: :played_at,
@@ -29,7 +32,11 @@ defmodule Minesweeper.Repo.Migrations.InitialSchema do
     end
 
     create constraint(:games, :dimensions_are_valid, check: "width >= 1 AND height >= 1")
-    create constraint(:games, :bombs_fit_on_board, check: "array_length(bombs, 1) < width * height - 1")
+
+    create constraint(:games, :bombs_fit_on_board,
+             check: "array_length(bombs, 1) < width * height - 1"
+           )
+
     create index(:moves, [:game_id, :played_at])
   end
 end
