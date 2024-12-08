59afb9bb27e9bd9d10d7433f537e8e6edfe514fc
Simon Oulevay <simon.oulevay@gmail.com>
2024-12-09 00:36:41 +0100

    2.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Minesweeper.MixProject do
   def project do
     [
       app: :minesweeper,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
